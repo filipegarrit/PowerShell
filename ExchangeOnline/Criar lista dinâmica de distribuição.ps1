58c0bce583cfb91b3ed86fe5000d70f576040270
FilipeGarrit <75532227+FilipeGarrit@users.noreply.github.com>
2024-09-04 14:06:20 -0300

    Update Criar lista dinâmica de distribuição.ps1

--- ExchangeOnline/Criar lista dinâmica de distribuição.ps1
+++ ExchangeOnline/Criar lista dinâmica de distribuição.ps1
@@ -1,3 +1,11 @@
-﻿$Filter = "((City -eq 'Rio de Janeiro') -and (-not(RecipientTypeDetailsValue -eq 'RoomMailbox')) -and (RecipientType -eq 'UserMailbox') -and (RecipientTypeDetails -ne 'GuestMailUser') -and (ExchangeUserAccountControl -ne 'AccountDisabled'))"
+#Limpar a console do POSH
+Clear-Host
 
-New-DynamicDistributionGroup -Name "Eletronuclear - SEDE" -DisplayName "Eletronuclear - SEDE" -Alias Eletronuclear-SEDE -PrimarySmtpAddress Eletronuclear-SEDE@eletrobrastermonuclear.onmicrosoft.com -RecipientFilter $Filter
+#Conectar no Exchange Online
+Connect-ExchangeOnline
+
+#Filtro para que o grupo dinâmico não inclua os parâmetros listados.
+$Filter = "((City -eq 'City') -and (-not(RecipientTypeDetailsValue -eq 'RoomMailbox')) -and (RecipientType -eq 'UserMailbox') -and (RecipientTypeDetails -ne 'GuestMailUser') -and (ExchangeUserAccountControl -ne 'AccountDisabled'))"
+
+#Criar o grupo atribuindo Nome, Nome de Exibição, Alias e SMTP primário, utilizando o Filtro acima como parâmetro para os membros a serem incluídos
+New-DynamicDistributionGroup -Name 'Group' -DisplayName 'GroupName' -Alias 'Alias' -PrimarySmtpAddress 'PrimarySMTP' -RecipientFilter $Filter
