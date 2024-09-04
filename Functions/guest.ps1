d44a66100ebb7bafa5e9c20deef5ebe9642ef255
Filipe Garrit <75532227+FilipeGarrit@users.noreply.github.com>
2024-09-04 16:37:22 -0300

    Add files via upload

---
+++ Functions/guest.ps1
@@ -0,0 +1,33 @@
+﻿#Função guest. Usada para exibir um convidado do Tenant na GAL.
+
+Function guest
+
+{
+    #Conecta no AzureAD
+    Connect-AzureAD
+
+    #Conecta no Exchange Online
+    Connect-ExchangeOnline
+
+    #Limpa a console
+    Clear-Host
+
+    #Solicita o ID Azure AD do usuário.
+    $objectId = Read-Host -Prompt 'Informe o ID do Usuário'
+
+    #Solicita o e-mail #EXT# do convidado.
+    $mailUser = Read-Host -Prompt 'Informe o e-mail do usuário Convidado'
+
+    #Exibir as informações do usuário com base no ID
+    Get-AzureADUser -ObjectId $objectId | Format-Table
+
+    #Pausa para confirmar se o usuário está correto
+    Pause
+
+    #Configura a Exibição do usuário no Catálogo de Endereços do Azure
+    Set-AzureADUser -ObjectId $objectId -ShowInAddressList $True
+
+    #Configura a Exibição do usuário no Catálogo de Endereços do Exchange. OBS: usar atributo 'User principal name' como o exemplo ao lado - login_dominio.com.br#EXT#
+    Set-MailUser $mailUser -HiddenFromAddressListsEnabled $false
+
+ }
